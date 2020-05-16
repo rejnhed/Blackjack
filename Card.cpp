a8a8c5b99383808ab26e62f8b7c235cfc8a76247
rejnhed <blastprotect@gmail.com>
2020-05-17 00:57:21 +0300

    5/17 Changes

--- Card.cpp
+++ Card.cpp
@@ -1,5 +1,5 @@
-#include "Card.h"
 
+#include "Card.h"
 
 bool compare(const char* str1, const char* str2)
 {
@@ -15,105 +15,103 @@ bool compare(const char* str1, const char* str2)
     return true;
 }
 
+Card::Card()
+{
+    value = 0;
+    face = nullptr;
+    suit = nullptr;
+    strcpy_s(serial_number, 8, "Default");
+    drawn = false;
+}
 
+Card::~Card()
+{
+    erase();
+}
 
-    Card::Card()
-    {
-        value = 0;
-        face = nullptr;
-        suit = nullptr;
-        strcpy_s(serial_number, 8, "Default");
-        drawn = false;
-    }
+void Card::setFace(const char* f)
+{
+    this->face = new char[strlen(f) + 1];
+    strcpy_s(this->face, strlen(f) + 1, f);
+}
 
-    Card::~Card()
+const char* Card::getFace() const
+{
+    return this->face;
+}
+
+void Card::setSuit(const char* s)
+{
+    suit = new char[strlen(s) + 1];
+    strcpy_s(this->suit, strlen(s) + 1, s);
+}
+
+const char* Card::getSuit() const
+{
+    return suit;
+}
+
+void Card::setSerial_number(const char* s)
+{
+    strcpy_s(this->serial_number, strlen(s) + 1, s);
+}
+
+const char* Card::getSerial_number() const
+{
+    return serial_number;
+}
+
+void Card::setValue(int v)
+{
+    value = v;
+}
+
+int Card::getValue() const
+{
+    return value;
+}
+
+void Card::setDrawn(bool d)
+{
+    drawn = d;
+}
+
+bool Card::getDrawn() const
+{
+    return drawn;
+}
+
+void Card::print() const
+{
+    cout << face << " of " << suit << " Ser#: " << getSerial_number() << endl;
+}
+
+Card& Card::operator=(const Card& other) {
+    if (this != &other)
     {
         erase();
+        copy(other);
     }
+    return *this;
+}
 
-    void Card::setFace(const char* f)
-    {
-        this->face = new char[strlen(f) + 1];
-        strcpy_s(this->face, strlen(f) + 1, f);
-    }
+Card::Card(const Card& other) {
+    this->copy(other);
+}
 
-    const char* Card::getFace() const
-    {
-        return this->face;
-    }
+void Card::copy(const Card& other)
+{
+    this->value = other.value;
+    face = new char[strlen(other.face) + 1];
+    strcpy_s(face, strlen(other.face) + 1, other.face);
+    suit = new char[strlen(other.suit) + 1];
+    strcpy_s(suit, strlen(other.suit) + 1, other.suit);
+    //serial_number = new char[strlen(other.serial_number) + 1];
+    strcpy_s(serial_number, strlen(other.serial_number) + 1, other.serial_number);
+    this->drawn = other.drawn;
+}
 
-    void Card::setSuit(const char* s)
-    {
-        suit = new char[strlen(s) + 1];
-        strcpy_s(this->suit, strlen(s) + 1, s);
-    }
-
-    const char* Card::getSuit() const
-    {
-        return suit;
-    }
-
-    void Card::setSerial_number(const char* s)
-    {
-        strcpy_s(this->serial_number, strlen(s) + 1, s);
-    }
-
-    const char* Card::getSerial_number() const
-    {
-        return serial_number;
-    }
-
-    void Card::setValue(int v)
-    {
-        value = v;
-    }
-
-    int Card::getValue() const
-    {
-        return value;
-    }
-
-    void Card::setDrawn(bool d)
-    {
-        drawn = d;
-    }
-
-    bool Card::getDrawn() const
-    {
-        return drawn;
-    }
-
-    void Card::print() const
-    {
-        cout << face << " of " << suit << " Ser#: " << getSerial_number() << endl;
-    }
-
-    Card& Card::operator=(const Card& other) {
-        if (this != &other)
-        {
-            erase();
-            copy(other);
-        }
-        return *this;
-    }
-
-    Card::Card(const Card& other) {
-        this->copy(other);
-    }
-
-    void Card::copy(const Card& other)
-    {
-        this->value = other.value;
-        face = new char[strlen(other.face) + 1];
-        strcpy_s(face, strlen(other.face) + 1, other.face);
-        suit = new char[strlen(other.suit) + 1];
-        strcpy_s(suit, strlen(other.suit) + 1, other.suit);
-        //serial_number = new char[strlen(other.serial_number) + 1];
-        strcpy_s(serial_number, strlen(other.serial_number) + 1, other.serial_number);
-        this->drawn = other.drawn;
-    }
-
-    void Card::erase() {
-        delete[] this->face;
-        delete[] this->suit;
-    }
+void Card::erase() {
+    delete[] this->face;
+    delete[] this->suit;
+}
--- Player.cpp
+++ Player.cpp
@@ -1,111 +1,111 @@
 
 #include "Player.h"
 
-    Player::Player()
-    {
-        first_name = nullptr;
-        second_name = nullptr;
-        age = 0;
-        wins = 0;
-        CW = 0.0;
-        CP = 0;
-        current_cards = nullptr;
-        cc_buffer = 0;
-        ace_buffer = 0;
-    }
+Player::Player()
+{
+    first_name = nullptr;
+    second_name = nullptr;
+    age = 0;
+    wins = 0;
+    CW = 0.0;
+    CP = 0;
+    current_cards = nullptr;
+    cc_buffer = 0;
+    ace_buffer = 0;
+}
 
-    void Player::setFirstName(const char* name1)
-    {
-        first_name = new char[strlen(name1) + 1];
-        strcpy_s(first_name, strlen(name1) + 1, name1);
-    }
+void Player::setFirstName(const char* name1)
+{
+    first_name = new char[strlen(name1) + 1];
+    strcpy_s(first_name, strlen(name1) + 1, name1);
+}
 
-    char* Player::getFirstName() const
-    {
-        return first_name;
-    }
+char* Player::getFirstName() const
+{
+    return first_name;
+}
 
-    short int Player::getAceB()
-    {
-        return ace_buffer;
-    }
+short int Player::getAceB()
+{
+    return ace_buffer;
+}
 
-    void Player::incrementAceB()
-    {
-        ace_buffer++;
-    }
+void Player::incrementAceB()
+{
+    ace_buffer++;
+}
 
-    void Player::decrementAceB()
-    {
-        ace_buffer--;
-    }
+void Player::decrementAceB()
+{
+    ace_buffer--;
+}
 
-    void Player::setSecondName(const char* name2)
-    {
-        second_name = new char[strlen(name2) + 1];
-        strcpy_s(second_name, strlen(name2) + 1, name2);
-    }
+void Player::setSecondName(const char* name2)
+{
+    second_name = new char[strlen(name2) + 1];
+    strcpy_s(second_name, strlen(name2) + 1, name2);
+}
 
-    char* Player::setSecondName() const
-    {
-        return second_name;
-    }
+char* Player::setSecondName() const
+{
+    return second_name;
+}
 
-    void Player::setCP(int cp)
-    {
-        CP += cp;
-    }
+void Player::setCP(int cp)
+{
+    CP += cp;
+}
 
-    int Player::getCP() const
-    {
-        return CP;
-    }
+int Player::getCP() const
+{
+    return CP;
+}
 
-    void Player::setWins(int w)
-    {
-        wins = w;
-    }
+void Player::setWins(int w)
+{
+    wins = w;
+}
 
-    int Player::getWins() const
-    {
-        return wins;
-    }
+int Player::getWins() const
+{
+    return wins;
+}
 
-    size_t Player::getCCB() const
-    {
-        return cc_buffer;
-    }
+size_t Player::getCCB() const
+{
+    return cc_buffer;
+}
 
-    void Player::printPlayer() const
-    {
-        cout << getFirstName() << " " << setSecondName() << " "
-            << getWins() << " " << getCP() << endl;
-    }
+void Player::printPlayer() const
+{
+    cout << getFirstName() << " " << setSecondName() << " "
+        << getWins() << " " << getCP() << endl;
+}
 
-    void Player::printCardBuffer() const
+void Player::printCardBuffer() const
+{
+    cout << "-----------------------------------\n";
+    cout << getFirstName() << " " << setSecondName() << "'s cards\n";
+    for (size_t i = 0; i < cc_buffer; i++)
     {
-        cout << "-----------------------------------\n";
-        cout << getFirstName() << " " << setSecondName() << "'s cards\n";
-        for (size_t i = 0; i < cc_buffer; i++)
-        {
-            current_cards[i].print();
-        }
-        cout << "Current points: " << getCP() << endl;
-        cout << "-----------------------------------\n";
+        current_cards[i].print();
     }
+    cout << "Current points: " << getCP() << endl;
+    cout << "-----------------------------------\n";
+}
 
-    void Player::addCard(Card card)
+void Player::addCard(Card card)
+{
+    Card* buffer_cards = new Card[cc_buffer + 1];
+    for (size_t i = 0; i < cc_buffer; i++)
     {
-        Card* buffer_cards = new Card[cc_buffer + 1];
-        for (size_t i = 0; i < cc_buffer; i++)
-        {
-            buffer_cards[i] = current_cards[i];
-        }
-        delete[] current_cards;
-        buffer_cards[cc_buffer] = card;
-        setCP(buffer_cards[cc_buffer].getValue());
-        if (compare(buffer_cards[cc_buffer].getFace(), "Ace"))
-            incrementAceB();
-        current_cards = buffer_cards;
-        cc_buffer++;
+        buffer_cards[i] = current_cards[i];
     }
+    delete[] current_cards;
+    buffer_cards[cc_buffer] = card;
+    setCP(buffer_cards[cc_buffer].getValue());
+    if (compare(buffer_cards[cc_buffer].getFace(), "Ace"))
+        incrementAceB();
+    current_cards = buffer_cards;
+    cc_buffer++;
+}
